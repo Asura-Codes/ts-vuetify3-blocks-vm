d21b9637c890790b62ea36d34b1aa5e746c85a32
patryk_tomaszewski <patryk_tomaszewski@dev.war.iase>
2023-07-15 15:44:27 +0200

    compile + run webassembly runtime test

--- vm-emscripten/makeDebug.ps1
+++ vm-emscripten/makeDebug.ps1
@@ -10,5 +10,5 @@ if(![System.IO.Directory]::Exists($FolderName))
 emcc src/vm/vm.c -c -o dist/vm.o
 emcc src/vm/vm-ops.c -c -o dist/vm-ops.o
 emcc -std=c++14 src/main.cpp -c -o dist/main.o
-emcc -lembind dist/vm.o dist/vm-ops.o dist/main.o -sEXPORTED_RUNTIME_METHODS="cwrap,ccall" -sEXPORT_ES6=0 -sMODULARIZE=1 -sEXPORT_NAME=VM -sUSE_ES6_IMPORT_META=1 -sERROR_ON_UNDEFINED_SYMBOLS=0 -sALLOW_MEMORY_GROWTH=1 -o dist/vm.html        # TESTS
+emcc -lembind --ts-typings dist/vm.o dist/vm-ops.o dist/main.o -sEXPORTED_RUNTIME_METHODS="cwrap,ccall,setValue,getValue" -sEXPORTED_FUNCTIONS='_malloc' -sEXPORT_ES6=0 -sMODULARIZE=1 -sEXPORT_NAME=VM -sUSE_ES6_IMPORT_META=1 -sERROR_ON_UNDEFINED_SYMBOLS=0 -sALLOW_MEMORY_GROWTH=1 -sIMPORTED_MEMORY=1 -o dist/vm.html        # TESTS
 # emcc -O3 -lembind dist/vm.o dist/vm-ops.o dist/main.o -sEXPORTED_RUNTIME_METHODS="cwrap,ccall" -sEXPORT_ES6=1 -sMODULARIZE=1 -sUSE_ES6_IMPORT_META=1 -sERROR_ON_UNDEFINED_SYMBOLS=0 -sALLOW_MEMORY_GROWTH=1 -o dist/vm.html  # DEPLOY
--- vm-emscripten/tests/execute.ts
+++ vm-emscripten/tests/execute.ts
@@ -1,7 +1,15 @@
-import * as VM from '../dist/vm.js'
+import * as VM from '../dist/vm'
+import { readFileSync } from 'fs'
 
   
 VM().then(vm => {
     const sum = vm.Add(5, 6);
     console.log(sum); // Outputs: 11
+
+    const code = readFileSync('examples/add.in')
+
+    console.log(typeof code)
+
+    vm.print_message(code);
 })
+ 
